c5fbbf281f8889f8bc6c167b9b409cd209129a47
sohil <sohilsfc24@gmail.com>
2023-10-02 18:09:57 +0530

    preloaded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import Project from './Components/Project/Project'
-import Skills from './Components/Skills/Skills';
-import About from './Components/About/About';
-import Contact from './Components/Contact/Contact';
-import Footer from './Components/Footer/Footer';
-import './App.css';
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import Project from "./Components/Project/Project";
+import Skills from "./Components/Skills/Skills";
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <React.Fragment>
-      <div className='container'>
-      <Navbar/>
-      <Hero/>
-      <Project/>
-      <Skills/>
-      <About/>
-      <Contact/>
+    <div>
+      <div className="container">
+        <Navbar />
+        <Hero />
+        <Project />
+        <Skills />
+        <About />
+        <Contact />
       </div>
-      <Footer/>
-    </React.Fragment>
+      <Footer />
+    </div>
   );
 }
 
